3b3516f6c8c2c80b1bc643cac2835031578b551c
merve yalçın <merveyalcin.01@gmail.com>
2024-12-10 13:14:40 +0300

    polishing up login activity

--- app/src/main/java/com/phi/smackchatapp/controller/LoginActivity.kt
+++ app/src/main/java/com/phi/smackchatapp/controller/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.phi.smackchatapp.controller
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.phi.smackchatapp.databinding.ActivityLoginBinding
@@ -17,20 +20,32 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginButtonClicked(view: View) {
+        enableSpinner(true)
         val email = binding.loginEmailText.text.toString()
         val password = binding.loginPasswordText.text.toString()
 
-        AuthService.loginUser(this, email, password) { success ->
-            if(success) {
-                AuthService.findUserByEmail(this) { findSuccess ->
-                    if(findSuccess) {
-                        finish()
+        hideKeyboard()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) { success ->
+                if (success) {
+                    AuthService.findUserByEmail(this) { findSuccess ->
+                        if (findSuccess) {
+                            finish()
+                        } else {
+                            errorToast()
+                        }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Please fill in both email and password!", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -38,5 +53,29 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this, CreateUserActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(isEnabled: Boolean) {
+
+        if(isEnabled) {
+            binding.loginSpinner.visibility = View.VISIBLE
+        } else {
+            binding.loginSpinner.visibility = View.INVISIBLE
+        }
+
+        binding.loginButton.isEnabled = !isEnabled
+        binding.loginCreateUserButton.isEnabled = !isEnabled
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
